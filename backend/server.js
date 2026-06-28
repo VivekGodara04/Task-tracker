20750114fa86b038e34fb12c43af6aa2dd16566b
Vivek Godara <vivekgodara04@gmail.com>
2026-06-28 21:26:31 +0530

    Fix CORS - remove option wildcard for Express 5

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('*', cors());
+
 app.use(express.json());
 
 // Routes
